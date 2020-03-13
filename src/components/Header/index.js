a7c6827559b4526cba890bb7afb8c7c2f24a40eb
Anisa <anisahamohamed@gmail.com>
2020-03-12 22:19:38 -0400

    added typewriter to description

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ function Header({ config }) {
           <HeaderImage/>
         )}
         <H1 class="line-1 anim-typewriter"><Link to="/">{author}</Link></H1>
-        <P>{description}</P>
+        <P class="line-1 anim-typewriter">{description}</P>
         {social &&
           <Social
             website={social.website}
