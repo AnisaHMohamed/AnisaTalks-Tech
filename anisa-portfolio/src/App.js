cd819fc99ee091ccf86a27717b9866dc7e2cda6e
AnisaHMohamed <anisahamohamed@gmail.com>
2022-10-11 13:12:19 -0400

    stye: add header styles

--- anisa-portfolio/src/App.js
+++ anisa-portfolio/src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
 import "./App.css"
+import Header from './components/Header';
 
 function App()
 {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <a
-          className="logo"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Anisa Talks Tech
-        </a>
-        <ul>
-        <li>
-        <a href="#">Home</a>
-        </li>
-        <li>
-        <a href="#">About</a>
-        </li>
-        <li>
-        <a href="#">Work</a>
-        </li>
-        <li>
-        <a href="#">Contact</a>
-        </li>
-        </ul>
-
-      </header>
+      <Header/>
       <div className="App-body">
       </div>
     </div>
---
+++ anisa-portfolio/src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './style.css'
+function Header()
+{
+    return (
+        <header className="App-header">
+            <a
+                className="logo"
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Anisa Talks Tech
+            </a>
+            <ul>
+                <li>
+                    <a href="#" className="active">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Work</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </header>
+
+    )
+}
+
+export default Header
