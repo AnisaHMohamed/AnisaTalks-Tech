53f728f6f9244cc84e6c1a8a187e1600ac20b064
AnisaHMohamed <anisahamohamed@gmail.com>
2022-10-11 13:19:50 -0400

    add header component

--- anisa-portfolio/src/App.js
+++ anisa-portfolio/src/App.js
@@ -8,6 +8,12 @@ function App()
     <div className="App">
       <Header/>
       <div className="App-body">
+      <section>
+      <img src="stars.png" id="stars" alt="stars"/>
+      <img src="moon.png" id="moon" alt="moon"/>
+      <img src="mountains_behind.png" id="mountains_behind" alt="mountains_behind"/>
+      <img src="mountains_front.png" id="mountains_front" alt="mountains_front"/>
+      </section>
       </div>
     </div>
   );
