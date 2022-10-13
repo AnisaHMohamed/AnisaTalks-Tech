0dfab24ba3e9706a16dfba571274feac52fe0542
AnisaHMohamed <anisahamohamed@gmail.com>
2022-10-13 16:58:09 -0400

    fix: displaying animations floating bubble and glowing rings overlap

--- anisa-portfolio/src/components/CssLibrary/GlowingRingsOverlapAnimation/index.js
+++ anisa-portfolio/src/components/CssLibrary/GlowingRingsOverlapAnimation/index.js
@@ -4,12 +4,16 @@ import "./style.css";
 const GlowingRingsOverlap = () => {
   return (
     <>
-      <div>GlowingRingsOverlap</div>
+      
+      <div className="animation-display">
+      <p className="animation-name">GlowingRingsOverlap Animation</p>
+      <div className="animation-display-container">
       <div className="ringContainer">
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <div className="ring"></div>
-      </div>
+      <div className="ring"></div>
+      <div className="ring"></div>
+      <div className="ring"></div>
+    </div>      </div>
+    </div>
     </>
   );
 };
