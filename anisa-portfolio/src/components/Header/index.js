494fa5fa247893b9a46b66e1b62c78dab2cf2db5
AnisaHMohamed <anisahamohamed@gmail.com>
2022-10-13 17:57:19 -0400

    format: header using prettier

--- anisa-portfolio/src/components/Header/index.js
+++ anisa-portfolio/src/components/Header/index.js
@@ -1,7 +1,6 @@
 import "./style.css";
 
 const Header = ({ top, activeTab }) => {
-  
   return (
     <header style={top} className="App-header">
       <a className="logo" href="#" target="_blank" rel="noopener noreferrer">
@@ -9,19 +8,24 @@ const Header = ({ top, activeTab }) => {
       </a>
       <ul>
         <li>
-          <a href="/" className={activeTab  == 'Home' ? 'active' : ''}>
+          <a href="/" className={activeTab == "Home" ? "active" : ""}>
             Home
           </a>
         </li>
         <li>
-        <a href="/css" className={activeTab  == 'Css' ? 'active' : ''} >
-        Css Library</a>
+          <a href="/css" className={activeTab == "Css" ? "active" : ""}>
+            Css Library
+          </a>
         </li>
         <li>
-          <a href="#" className={activeTab  == 'Crow' ? 'active' : ''}>Crow Hunt</a>
+          <a href="#" className={activeTab == "Crow" ? "active" : ""}>
+            Crow Hunt
+          </a>
         </li>
         <li>
-          <a href="#" className={activeTab  == 'Crow' ? 'active' : ''}>Contact</a>
+          <a href="#" className={activeTab == "Crow" ? "active" : ""}>
+            Contact
+          </a>
         </li>
       </ul>
     </header>
